b85d949bb29711fcd9e9d8451d57dba39706fa70
Johandika <johanelyosse@gmail.com>
2023-07-11 09:23:24 +0700

    React Native Job Seeker Apps

---
+++ hook/useFetch.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    params: { ...query },
+    headers: {
+      "X-RapidAPI-Key": "bbf8354036mshfe0194e4200c586p15cc1djsn02428f09a3f5",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const res = await axios.request(options);
+
+      setData(res.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
